674ddda001c8c9cefc48199b4b83320a9cfb88bb
Constanza <coe.elena47@gmail.com>
2022-11-23 10:27:46 -0300

    added the rought notation

--- js/script.js
+++ js/script.js
@@ -16,29 +16,47 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+;
 
-import { annotate } from 'https://unpkg.com/rough-notation?module';
+const n1 = document.querySelector('.highlight1');
+const n2 = document.querySelector('.highlight2');
+const n3 = document.querySelector('.highlight3');
+const n4 = document.querySelector('.highlight4');
+const n5 = document.querySelector('.highlight5');
+const n6 = document.querySelector('.aboutme');
+const n7 = document.querySelector('.projects')
+const n8 = document.querySelector('.education');
+const n9 = document.querySelector('.contact')
 
-const e = document.querySelector('em');
-const annotation = annotate(e, { type: 'underline' });
-annotation.show();
+let roughElements = []
+let seconds = 0;
 
-// import { annotate } from 'https://unpkg.com/rough-notation?module';
+roughElements.push(RoughNotation.annotate(n6, { type: 'highlight', color: 'lavender' , multiline: true}));
+roughElements.push(RoughNotation.annotate(n7, { type: 'highlight', color: 'gold' , multiline: true}));
+roughElements.push(RoughNotation.annotate(n8, { type: 'highlight', color: 'mistyrose' , multiline: true}));
+roughElements.push(RoughNotation.annotate(n9, { type: 'highlight', color: 'powderblue' , multiline: true}));
+roughElements.push(RoughNotation.annotate(n1, { type: 'highlight', color:'lavender', iterations: 4, multiline: true }));
+roughElements.push(RoughNotation.annotate(n3, { type: 'highlight', color: 'mistyrose', iterations: 4, multiline: true}));
+roughElements.push(RoughNotation.annotate(n4, { type: 'highlight', color: 'gold', iterations: 4, multiline: true}))
+roughElements.push(RoughNotation.annotate(n2, { type: 'highlight', color:'powderblue', iterations: 4, multiline: true}));
+roughElements.push(RoughNotation.annotate(n5, { type: 'highlight', color:'lavender', iterations: 4, multiline: true }));
 
-// const n1 = document.querySelector('em');
-// const n2 = document.querySelector('strong');
-// const n3 = document.querySelector('h1');
-// const n4 = document.querySelector('span');
-// const n5 = document.querySelector('#block');
 
-// const a1 = annotate(n1, { type: 'underline', color: 'blue' });
-// const a2 = annotate(n2, { type: 'circle', color: 'red', padding: 10 });
-// const a3 = annotate(n3, { type: 'box', color: 'orange' });
-// const a4 = annotate(n4, { type: 'highlight', color: 'yellow', iterations: 1, multiline: true });
-// const a5 = annotate(n5, { type: 'bracket', color: 'red', padding: [2, 10], brackets: ['left', 'right'], strokeWidth: 3 })
+
+roughElements.forEach( (item) => {
+  setTimeout( () =>{
+    item.show();
+  }, seconds)
+  seconds = seconds + 700;
+})
 
 // a1.show();
 // a2.show();
 // a3.show();
 // a4.show();
 // a5.show();
+// a6.show();
+// a7.show();
+// a8.show();
+// a9.show();
+
